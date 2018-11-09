534322f7e596602850fb87692a7fbdcdfdbdb0a8
Victor Baranov <baranov.viktor.27@gmail.com>
2018-11-09 13:45:39 +0300

    Support of EIP-1102

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import Button from 'react-bootstrap/lib/Button'
-import logo from './logo.svg'
 import Web3 from 'web3'
 import './App.css'
 import config from './config.js'
@@ -14,7 +13,7 @@ class App extends Component {
     this.web3 = null
     this.EventEmitterContractInstance = null
     this.state = {
-      "isLoading": false
+      "isLoading": false,
     }
   }
 
@@ -31,7 +30,7 @@ class App extends Component {
               <i>Note:</i> Smart-contracts are deployed to POA core and Sokol networks.
             </h4>
           </h2>
-          <Button onClick={() => this.executeContract()} disabled={this.state.isLoading}>Execute Contract</Button>
+          <Button onClick={() => this.executeContract()} disabled={this.state.isLoading || this.state.isRejected || (this.state.networkID !== 77 && this.state.networkID !== 99)}>Execute Contract</Button>
           {this.renderLoader()}
           <ToastContainer />
         </header>
@@ -51,23 +50,12 @@ class App extends Component {
     )
   }
 
-  componentDidMount = () => {
-    if (window.web3) {
+  componentWillMount = () => {
+    if (window.ethereum) {
+        this._getWeb3()
+    } else if (window.web3) {
         this.web3 = new Web3(window.web3.currentProvider)
-        const EventEmitterContract = this.web3.eth.contract(config.contract.abi)
-        const networkID = this.web3.version.network;
-        this.EventEmitterContractInstance = EventEmitterContract.at(config.contract.addresses[networkID])
-        const contractEvent = this.EventEmitterContractInstance.FunctionCalled()
-        contractEvent.watch((error, result) => {
-          this.setState({
-            "isLoading": false
-          })
-          if (error) {
-            console.log(error)
-          }
-          console.log(result)
-          this.notify(`Event has been successfully emitted with id ${result.args._id} !`)
-        })
+        this._subscribeToEvent()
     }
   }
 
@@ -82,14 +70,54 @@ class App extends Component {
     }, (error, txHash) => {
       if (error) {
         console.log(error)
+        this.setState({
+          "isLoading": false
+        })
       }
-      console.log(`txHash: ${txHash}`)
+      if (txHash) {
+        console.log(`txHash: ${txHash}`)
+      }
     })
   }
 
-  notify = (message) => toast(message);
+  notify = (message) => toast(message)
 
-  _getRandomInt(min, max) {
+  _getWeb3 = async () => {
+    this.web3 = new Web3(window.ethereum);
+    try {
+        // Request account access if needed
+        await window.ethereum.enable()
+        // Acccounts now exposed
+        this._subscribeToEvent()
+    } catch (error) {
+        // User denied account access...
+        this.setState({
+          "isRejected": true
+        })
+    }
+  }
+
+  _subscribeToEvent = () => {
+    const EventEmitterContract = this.web3.eth.contract(config.contract.abi)
+    const networkID = this.web3.version.network;
+    this.EventEmitterContractInstance = EventEmitterContract.at(config.contract.addresses[networkID])
+    const contractEvent = this.EventEmitterContractInstance.FunctionCalled()
+    this.setState({
+      "networkID": Number(networkID)
+    })
+    contractEvent.watch((error, result) => {
+      this.setState({
+        "isLoading": false
+      })
+      if (error) {
+        console.log(error)
+      }
+      console.log(result)
+      this.notify(`Event has been successfully emitted with id ${result.args._id} !`)
+    })
+  }
+
+  _getRandomInt = (min, max) => {
     return Math.floor(Math.random() * (max - min)) + min;
   }
 }
